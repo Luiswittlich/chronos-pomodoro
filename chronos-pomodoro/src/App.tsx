b0208f69236d715afc9a3b3403b9cac86b8613cf
Luis Carlos <luiscwj@gmail.com>
2026-01-29 10:46:38 -0300

    Criando Container.module e css

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,28 +2,28 @@
 //App
 //ExemploDeComponente
 
-import { Heading } from './components/Heading';
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
 
   return (//Podemos utilizar <Fragment> </Fragment> como elemento pai também, mesma coisa que <> </>.
     <> 
-      <Heading>
-        Olá, Mundo! 
-        <button><TimerIcon/></button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni dolores
-        iusto similique ipsam non modi placeat voluptates repellat itaque,
-        maxime hic in temporibus ab voluptatum libero voluptatem consectetur
-        sapiente saepe!
-      </p>
-      <p>Realizando testes!!</p>
+      <Container>
+          <Heading>Testando Meu componente Heading</Heading>
+          <section>LOGO</section>
+        </Container>
+      <Container>
+          <section>MENU</section>
+        </Container>
+      <Container>
+          <section>FORM</section>
+        </Container>
+      <Container>
+          <section>FORM</section>
+        </Container>
     </>
   );
 }
