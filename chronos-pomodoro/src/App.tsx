3c743dda86f036f098c02e2f749d8a8d046399af
Luis Carlos <luiscwj@gmail.com>
2026-01-30 10:22:39 -0300

    Criando CountDown.tsx

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -3,27 +3,30 @@
 //ExemploDeComponente
 
 import { Container } from './components/Container/Index';
-import { Heading } from './components/Heading/Index';
 import { Logo } from './components/Logo/Index';
+import { Menu } from './components/Menu/Index';
+import { CountDown } from './components/CountDown/Index';
+
 
 import './styles/theme.css';
 import './styles/global.css';
+
+
 
 export function App() {
   return (
     //Podemos utilizar <Fragment> </Fragment> como elemento pai também, mesma coisa que <> </>.
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
+      
       <Container>
-        <Heading>MENU</Heading>
+        <Menu/>
       </Container>
+      
       <Container>
-        <section>FORM</section>
-      </Container>
-      <Container>
-        <section>FORM</section>
+        <CountDown/>
       </Container>
     </>
   );
