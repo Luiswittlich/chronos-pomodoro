e034e11966ba7ff8f2e5ab77117d119e4bdcd7b2
Luis Carlos <luiscwj@gmail.com>
2026-02-15 10:19:23 -0300

    Criando context

--- chronos-pomodoro/src/components/CountDown/Index.tsx
+++ chronos-pomodoro/src/components/CountDown/Index.tsx
@@ -1,7 +1,9 @@
 
+import { useTaskContext } from '../../contexts/TaskContext'
 import styles from './styles.module.css'
 
 
 export function CountDown(){
+    const TaskContext = useTaskContext
     return <div className={styles.container}>00:00</div>
 }
--- chronos-pomodoro/src/contexts/TaskContext/index.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/index.tsx
@@ -1,5 +1,43 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
-export const TaskContext = createContext({
-    chave: 'valor'
-})
+const initialState:TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsReamining: '00:00',
+  activeTask:null,
+  currentCycle: 0,
+  config:{
+    workTime: 25,
+    shortBreakTime:5,
+    longBreakTime:15,
+  },
+}
+type TaskContextProps = {
+    state: TaskStateModel;
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+}
+
+
+const initialContextValue =  {
+    state: initialState,
+    setState: ()=>{},
+}
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue)
+
+type TaskContextPrviderProps = {
+    children: React.ReactNode,
+}
+
+export function TaskContextProvider({children}: TaskContextPrviderProps){
+    return (
+        <TaskContext.Provider value={initialContextValue}>
+            {children}
+        </TaskContext.Provider>
+    )
+}
+
+export function useTaskContext() {
+    return useContext(TaskContext);
+}
