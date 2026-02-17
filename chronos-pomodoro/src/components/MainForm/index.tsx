5a1843d6146c9b02cfaa620c32d6085d6983a50c
Luis Carlos <luiscwj@gmail.com>
2026-02-17 06:42:26 -0300

    Criando nova tarefa

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -4,19 +4,65 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 
 export function MainForm(){
+  const { setState } = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement>(null)
+    
+  
+
   function handleCreateNewTask(event:React.SubmitEvent<HTMLFormElement>){
     event.preventDefault();
-    console.log('deu certo')
+    
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Por favor, digite um nome válido!')
+      return
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name:taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate:null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+          config: {...prevState.config},
+          activeTask:newTask,
+          currentCycle: 1, //Conferir
+          secondsRemaining: secondsRemaining,
+          formattedSecondsReamining: '00:00',
+          tasks:[...prevState.tasks, newTask],
+      }
+    })
   }
 
 
     return(
         <form onSubmit={handleCreateNewTask} className={styles.form}>
           <div className={styles.formRow}>
-            <DefaultInput labelText='Task:' id='input' type='text' placeholder='Digite algo...' />
+            <DefaultInput 
+              labelText='Task:' 
+              id='input' 
+              type='text' 
+              placeholder='Digite algo...' 
+              ref={taskNameInput}
+            />
           </div>
 
           <div className={styles.formRow}>
